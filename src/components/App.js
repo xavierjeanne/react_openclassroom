94ebbc02aabdd81f4f24474eef2c5a25571d84ab
xavierjeanne <xavier.jeanne@imageinfrance.com>
2023-03-28 17:40:32 +0200

    add props children

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
-import '../App.css';
 import Banner from './Banner'
-import Cart from './Cart'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
-    return <div> <Banner/> <Cart/> </div>
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default App;
+export default App

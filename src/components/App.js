f7ca2433b4aa9b91cac95389b2f165390cf2a7f2
xavierjeanne <xavier.jeanne@imageinfrance.com>
2023-04-03 17:21:11 +0200

    add input email control

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
+import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 
 function App() {
@@ -10,8 +11,10 @@ function App() {
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+
 			{/* <Cart /> */}
 			<ShoppingList />
+			<Footer />
 		</div>
 	)
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -17,15 +17,15 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-                {plantList.map(({ id, cover, name, water, light }) => (
-                    <PlantItem
-                        id={id}
-                        cover={cover}
-                        name={name}
-                        water={water}
-                        light={light}
-                    />
-                ))}
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						key={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
+				))}
 			</ul>
 		</div>
 	)
